9c89b5eb52a68fd22d30b7cd85a9249df07e5e25
Gabriel Walters <gwalters@lhschools.org>
2020-02-18 19:07:37 -0500

    intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,15 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("This is RedBeard");
+        System.out.println("Over the summer, I got certified to teach 7 College Credit Plus Classes");
+
+
+
+
+    }
+}
